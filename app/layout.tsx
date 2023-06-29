6ebedb290372a7505ea0acbad2fc95da2dd891d6
pikachu-78 <divyanjalisarabu@gmail.com>
2023-06-29 12:10:01 +0530

    initial

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import AuthContext from './context/AuthContext'
+import ToasterContext from './context/ToasterContext'
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'chatter',
+  description: 'chatter',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>  
+        </body>
+    </html>
+  )
+}
+
